59f1fee7e6c6a5438bc4aa7c42e858bab364142b
Simon Søllingvraa <simon@soellingvraa.dk>
2015-02-12 20:51:11 +0100

    Added game screen

--- PingEmDown/Screen/GameScreen.cs
+++ PingEmDown/Screen/GameScreen.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using PingEmDown.Level;
 using PingEmDown.Messaging.Caliburn.Micro;
 
 namespace PingEmDown.Screen
@@ -6,28 +7,34 @@ namespace PingEmDown.Screen
     public class GameScreen : IScreen
     {
         private readonly IEventAggregator _eventAggregator;
+        private readonly ILevelManager _levelManager;
 
-        public GameScreen(IEventAggregator eventAggregator)
+        public GameScreen(IEventAggregator eventAggregator, ILevelManager levelManager)
         {
             _eventAggregator = eventAggregator;
+            _levelManager = levelManager;
         }
 
         public void Load()
         {
             _eventAggregator.Subscribe(this);
+            _levelManager.Load();
         }
 
         public void Unload()
         {
+            _levelManager.Unload();
             _eventAggregator.Unsubscribe(this);
         }
 
         public void Update(GameTime gameTime)
         {
+            _levelManager.Update(gameTime);
         }
 
         public void Draw()
         {
+            _levelManager.Draw();
         }
     }
 }
