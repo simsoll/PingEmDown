05041dde05444797de646834a89e5a16bf2c0435
Simon Søllingvraa <simon@soellingvraa.dk>
2015-02-14 14:47:42 +0100

    Enabled randomization

---
+++ PingEmDown/Randomizer/IRandomizer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PingEmDown.Randomizer
+{
+    public interface IRandomizer
+    {
+        int Next();
+        int Next(int maxValue);
+        int Next(int minValue, int maxValue);
+        double NextDouble();
+    }
+}
---
+++ PingEmDown/Randomizer/Randomizer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PingEmDown.Randomizer
+{
+    public class Randomizer : IRandomizer
+    {
+        private readonly Random _random;
+
+        public Randomizer()
+        {
+            _random = new Random();
+        }
+
+        public Randomizer(Random random)
+        {
+            _random = random;
+        }
+
+        public Randomizer(int seed)
+        {
+            _random = new Random(seed);
+        }
+
+        public int Next()
+        {
+            return _random.Next();
+        }
+
+        public int Next(int maxValue)
+        {
+            return _random.Next(maxValue);
+        }
+
+        public int Next(int minValue, int maxValue)
+        {
+            return _random.Next(minValue, maxValue);
+        }
+
+        public double NextDouble()
+        {
+            return _random.NextDouble();
+        }
+    }
+}
